46c1c8d7ab488b85a0cc28a339239f0478a3ac3f
Ibn_misbah <kawnuma@gmail.com>
2025-07-13 19:49:45 +0300

    oredr_id -> order_date

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -25,7 +25,7 @@ CREATE TABLE Customers (
 CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
-    order_id DATE,
+    order_date DATE,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
