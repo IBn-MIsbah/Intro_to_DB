68812a58693e48f67b16943d295af5bd731636ff
Ibn_misbah <kawnuma@gmail.com>
2025-07-13 19:48:43 +0300

    ADDRESS -> address

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -19,7 +19,7 @@ CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
-    ADDRESS TEXT
+    address TEXT
 );
 
 CREATE TABLE Orders (
